458a0d7162416cc9ce608f4656bd291466507608
Eryk Kryszewski <eric9972@o2.pl>
2026-01-31 00:07:59 +0100

    homepage backend done

--- front-page.php
+++ front-page.php
@@ -8,26 +8,126 @@ get_header(); the_post();
     <div class="container-fluid container-fluid--padding" data-askee-page="home">
         <div class="askee-homepage__wrapper">
             <div class="askee-homepage__slider">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="askee-homepage__welcome">
-                            <h1>
-                                Cześć!
-                                <span>Jestem Twoim Asystentem Askee.</span>
-                            </h1>
+                <div class="askee-homepage__item">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="askee-homepage__welcome">
+                                <h1>
+                                    Cześć!
+                                    <span>Jestem Twoim Asystentem Askee.</span>
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-2">
+                            <div class="askee-homepage__content">
+                                <div class="askee-homepage__image"><?php echo wp_get_attachment_image(5055, 'large'); ?></div>
+                                <div class="askee-homepage__boxes">
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            <strong>Pomagam w codziennej pracy.</strong>
+                                            W jednym miejscu, bez przełączania się między systemami i aplikacjami.
+                                        </p>
+                                    </div>
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            Wystarczy, że zadasz pytanie lub zlecisz zadanie – odpowiem albo zrobię to za Ciebie. Szybko, prosto i w sposób
+                                            <strong>dopasowany do Twojej roli.</strong>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 col-lg-8 offset-lg-2">
-                        <div class="askee-homepage__content">
-                            <div class="askee-homepage__image"><?php echo wp_get_attachment_image(5055, 'large'); ?></div>
-                            <div class="askee-homepage__boxes">
-                                <div class="askee-homepage__box">
-                                    <p>Pomagam w codziennej pracy. W jednym miejscu, bez przełączania się między systemami i aplikacjami.</p>
+                <div class="askee-homepage__item">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="askee-homepage__welcome">
+                                <h2>Dopasuję się do Twoich potrzeb!</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-2">
+                            <div class="askee-homepage__content">
+                                <div class="askee-homepage__image"><?php echo wp_get_attachment_image(5055, 'large'); ?></div>
+                                <div class="askee-homepage__boxes">
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            <strong>Mogę wspierać Cię w różnych obszarach</strong>
+                                            – wybierz, w czym mam Ci pomóc. Dopasuję się do Ciebie.
+                                        </p>
+                                    </div>
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            Wspieram w sprawach kadrowych, rozwoju i ocenie pracowników, szybkim dostępie do wiedzy oraz zgodności z zasadami i procedurami firmy.
+                                            <strong>Zawsze działam w oparciu o kontekst,</strong>
+                                            rolę użytkownika i zasady Twojej organizacji.
+                                        </p>
+                                    </div>
                                 </div>
-                                <div class="askee-homepage__box">
-                                    <p>Wystarczy, że zadasz pytanie lub zlecisz zadanie –odpowiem albo zrobię to za Ciebie. Szybko, prosto i w sposób dopasowany do Twojej roli.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="askee-homepage__item">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="askee-homepage__welcome">
+                                <h2>Zawsze jestem przy Tobie!</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-2">
+                            <div class="askee-homepage__content">
+                                <div class="askee-homepage__image"><?php echo wp_get_attachment_image(5055, 'large'); ?></div>
+                                <div class="askee-homepage__boxes">
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            Jestem dostępny zawsze, gdy mnie potrzebujesz. Odpowiadam na pytania i wspieram Cię w pracy
+                                            <strong>bez przerw i bez czekania.</strong>
+                                        </p>
+                                    </div>
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            Możesz korzystać ze mnie w aplikacji ASKee oraz w narzędziach, których używasz na co dzień: na komputerze, tablecie, telefonie, w Teams, Slacku i innych systemach.
+                                            <strong>Jestem zawsze pod ręką!</strong>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="askee-homepage__item">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="askee-homepage__welcome">
+                                <h2>Twoje bezpieczeństwo to mój priorytet!</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-lg-8 offset-lg-2">
+                            <div class="askee-homepage__content">
+                                <div class="askee-homepage__image"><?php echo wp_get_attachment_image(5055, 'large'); ?></div>
+                                <div class="askee-homepage__boxes">
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            <strong>Nie wysyłam żadnych wrażliwych ani biznesowych danych</strong>
+                                            do chmury publicznej.
+                                        </p>
+                                    </div>
+                                    <div class="askee-homepage__box">
+                                        <p>
+                                            <strong>Anonimizuję i szyfruję wszystkie informacje,</strong>
+                                            które mi powierzysz. Twoje dane są w pełni chronione – zawsze i wszędzie są dostępne
+                                            <strong>wyłącznie między nami. </strong>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- js/src/pages/home.js
+++ js/src/pages/home.js
@@ -1,12 +1,43 @@
+import $ from "jquery";
+import "slick-carousel";
+
 export function initAskeeHomePage(rootElement) {
     const safeRootElement = rootElement instanceof Element ? rootElement : document;
-    const pageElement = safeRootElement.querySelector('[data-askee-page="home"]');
-    if (!pageElement) {
+    const homePageContainer = safeRootElement.querySelector('[data-askee-page="home"]');
+
+    if (!homePageContainer) {
         return;
     }
-    if (pageElement.dataset.askeeInitialized === "1") {
+
+    if (homePageContainer.dataset.askeeInitialized === "1") {
         return;
     }
-    pageElement.dataset.askeeInitialized = "1";
-    console.log("welcome on home");
+
+    homePageContainer.dataset.askeeInitialized = "1";
+
+    const homepageSliders = homePageContainer.querySelectorAll(".askee-homepage__slider");
+
+    homepageSliders.forEach((singleSlider) => {
+        if (singleSlider.dataset.askeeSlickInitialized === "1") {
+            return;
+        }
+
+        const jquerySliderInstance = $(singleSlider);
+
+        const slickConfiguration = {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            autoplay: false,
+            autoplaySpeed: 4000,
+            fade: true,
+            cssEase: "linear",
+        };
+
+        jquerySliderInstance.slick(slickConfiguration);
+
+        singleSlider.dataset.askeeSlickInitialized = "1";
+    });
 }
