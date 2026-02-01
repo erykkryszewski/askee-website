3c4bc2c0fa7d8e9d5a0816e748c003731b361c20
Eryk Kryszewski <eric9972@o2.pl>
2026-02-01 23:19:47 +0100

    site 75%

--- page-kontakt.php
+++ page-kontakt.php
@@ -1,14 +1,56 @@
 <?php
 
-get_header();
-the_post();
+get_header(); the_post();
 
-get_template_part("template-parts/app-nav");
 ?>
 
-<div class="container" data-askee-page="kontakt">
-    <h1>Kontakt</h1>
-    <p>To jest strona Kontakt.</p>
+<div class="askee-chat askee-contact">
+    <div class="container-fluid container-fluid--padding" data-askee-page="chat">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2 askee-chat__column askee-chat__column--mid">
+                <div class="askee-chat__wrapper">
+                    <h1 class="askee-chat__title">Koniec z „kto ma do tego dostęp?”.</h1>
+
+                    <div class="askee-chat__box">
+                        <div class="askee-chat__switch-sections">
+                            <div class="askee-chat__content askee-chat__content--default" id="askee-chat-content-default">
+                                <span class="askee-contact__welcome-wrapper">
+                                    <p class="askee-chat__welcome">Kontakt z nami:?</p>
+                                </span>
+                                <div>
+                                    <div class="askee-contact__content">
+                                        <div class="askee-contact__image"><?php echo wp_get_attachment_image(5056, 'large'); ?></div>
+                                        <div class="askee-contact__info">
+                                            <p>Askee Sp. z o.o.</p>
+                                            <p>ul. Gostyńska 91, 80-298 Gdańsk</p>
+                                            <p>NIP: 5223018301</p>
+                                            <a href="mailto:kontakt@askee.pl">Email: kontakt@askee.pl</a>
+                                            <a href="tel:+48500025365">Tel. 500 025 365</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <form class="askee-chat__form">
+                            <textarea class="askee-chat__textarea" name="user_message" placeholder="Masz pytania? Jesteś zainteresowany/-a ASKEE? Odezwij się do nas!" required></textarea>
+
+                            <button type="submit" class="askee-chat__submit"><?php echo wp_get_attachment_image(5070, "large"); ?></button>
+                        </form>
+                    </div>
+
+                    <div class="askee-chat__buttons">
+                        <a class="button button--light" href="/poznaj-mnie" data-id="askee-chat-content-meet">Poznaj mnie</a>
+                        <a class="button button--light" href="/obszary-wsparcia" data-id="askee-chat-content-areas">Obszary wsparcia</a>
+                        <a class="button button--light" href="/jak-moge-ci-pomoc" data-id="askee-chat-content-help">Jak mogę Ci pomóc?</a>
+                        <a class="button button--light" href="/warunki-wspolpracy" data-id="askee-chat-content-terms">Warunki współpracy</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-2 askee-chat__column askee-chat__column--right"><?php get_template_part("template-parts/sidebar"); ?></div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
--- page-nasza-filozofia.php
+++ page-nasza-filozofia.php
@@ -1,14 +1,88 @@
 <?php
 
-get_header();
-the_post();
+get_header(); the_post();
 
-get_template_part("template-parts/app-nav");
 ?>
 
-<div class="container" data-askee-page="nasza-filozofia">
-    <h1>Nasza filozofia</h1>
-    <p>To jest strona Nasza filozofia.</p>
+<div class="askee-chat askee-our-philosophy">
+    <div class="container-fluid container-fluid--padding" data-askee-page="chat">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2 askee-chat__column askee-chat__column--mid">
+                <div class="askee-chat__wrapper">
+                    <h1 class="askee-chat__title">Koniec z „kto ma do tego dostęp?”.</h1>
+
+                    <div class="askee-chat__box">
+                        <div class="askee-chat__switch-sections">
+                            <div class="askee-chat__content askee-chat__content--default" id="askee-chat-content-default">
+                                <span class="askee-our-philosophy__welcome-wrapper">
+                                    <p class="askee-chat__welcome">Chcesz poznać naszą filozofię?</p>
+                                    <button class="button button--ghost">Tak</button>
+                                </span>
+                                <div>
+                                    <div class="askee-our-philosophy__icons">
+                                        <div class="askee-our-philosophy__column askee-our-philosophy__column--left">
+                                            <div class="askee-our-philosophy__item">
+                                                <?php echo wp_get_attachment_image(5088, 'large'); ?>
+                                                <h4>Technologia</h4>
+                                            </div>
+
+                                            <div class="askee-our-philosophy__item">
+                                                <?php echo wp_get_attachment_image(5090, 'large'); ?>
+                                                <h4>Bezpieczeństwo</h4>
+                                            </div>
+                                            <div class="askee-our-philosophy__item">
+                                                <?php echo wp_get_attachment_image(5089, 'large'); ?>
+                                                <h4>Analiza</h4>
+                                            </div>
+                                        </div>
+                                        <div class="askee-our-philosophy__column askee-our-philosophy__column--mid">
+                                            <div class="askee-our-philosophy__item askee-our-philosophy__item--big"><?php echo wp_get_attachment_image(5091, 'large'); ?></div>
+                                        </div>
+                                        <div class="askee-our-philosophy__column askee-our-philosophy__column--right">
+                                            <div class="askee-our-philosophy__item">
+                                                <?php echo wp_get_attachment_image(5088, 'large'); ?>
+                                                <h4>Technologia</h4>
+                                            </div>
+
+                                            <div class="askee-our-philosophy__item">
+                                                <?php echo wp_get_attachment_image(5090, 'large'); ?>
+                                                <h4>Bezpieczeństwo</h4>
+                                            </div>
+                                            <div class="askee-our-philosophy__item">
+                                                <?php echo wp_get_attachment_image(5089, 'large'); ?>
+                                                <h4>Analiza</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <span class="askee-our-philosophy__welcome-wrapper">
+                                        <p>Chcesz wiedzieć jak przetwarzam dane?</p>
+                                        <button class="button button--ghost">Tak</button>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <form class="askee-chat__form">
+                            <textarea class="askee-chat__textarea" name="user_message" placeholder="Wyślij wiadomość do Askee..." required></textarea>
+
+                            <button type="submit" class="askee-chat__submit"><?php echo wp_get_attachment_image(5070, "large"); ?></button>
+                        </form>
+                    </div>
+
+                    <div class="askee-chat__buttons">
+                        <a class="button button--light" href="/poznaj-mnie" data-id="askee-chat-content-meet">Poznaj mnie</a>
+                        <a class="button button--light" href="/obszary-wsparcia" data-id="askee-chat-content-areas">Obszary wsparcia</a>
+                        <a class="button button--light" href="/jak-moge-ci-pomoc" data-id="askee-chat-content-help">Jak mogę Ci pomóc?</a>
+                        <a class="button button--light" href="/warunki-wspolpracy" data-id="askee-chat-content-terms">Warunki współpracy</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-2 askee-chat__column askee-chat__column--right"><?php get_template_part("template-parts/sidebar"); ?></div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
