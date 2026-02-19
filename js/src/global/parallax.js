e0b3b2761baf75ae574af766b17b95522e7b10e5
Eryk Kryszewski <eric9972@o2.pl>
2026-02-19 11:57:24 +0100

    stuff done

--- js/src/global/parallax.js
+++ js/src/global/parallax.js
@@ -2,42 +2,77 @@ export function initAskeeParallax() {
     const blobTop = document.querySelector(".askee-blob--top-right");
     const blobBottom = document.querySelector(".askee-blob--bottom-left");
 
-    if (!blobTop || !blobBottom) {
-        return;
-    }
+    if (!blobTop || !blobBottom) return;
+
+    const mq = window.matchMedia("(min-width: 1200px)");
 
     let targetX = 0;
     let targetY = 0;
     let currentX = 0;
     let currentY = 0;
     let rafId = null;
+    let enabled = false;
 
     function onMouseMove(e) {
         targetX = e.clientX / window.innerWidth - 0.5;
         targetY = e.clientY / window.innerHeight - 0.5;
     }
 
-    function tick() {
-        currentX += (targetX - currentX) * 0.1;
-        currentY += (targetY - currentY) * 0.1;
-
+    function applyTransform() {
         const moveX = currentX * 50;
         const moveY = currentY * 50;
 
         blobTop.style.transform = `translate3d(${moveX}px, ${moveY}px, 0)`;
-
         blobBottom.style.transform = `translate3d(${-moveX}px, ${-moveY}px, 0)`;
+    }
 
+    function tick() {
+        if (!enabled) return;
+
+        currentX += (targetX - currentX) * 0.1;
+        currentY += (targetY - currentY) * 0.1;
+
+        applyTransform();
         rafId = requestAnimationFrame(tick);
     }
 
-    // start
-    window.addEventListener("mousemove", onMouseMove);
-    tick();
+    function enable() {
+        if (enabled) return;
 
-    // cleanup (przy przeładowaniu strony)
-    return function cleanup() {
+        enabled = true;
+        targetX = 0;
+        targetY = 0;
+        currentX = 0;
+        currentY = 0;
+
+        window.addEventListener("mousemove", onMouseMove, { passive: true });
+        applyTransform();
+        tick();
+    }
+
+    function disable() {
+        if (!enabled) return;
+
+        enabled = false;
         window.removeEventListener("mousemove", onMouseMove);
+
         if (rafId) cancelAnimationFrame(rafId);
+        rafId = null;
+
+        blobTop.style.transform = "translate3d(0, 0, 0)";
+        blobBottom.style.transform = "translate3d(0, 0, 0)";
+    }
+
+    function handleChange(e) {
+        if (e.matches) enable();
+        else disable();
+    }
+
+    handleChange(mq);
+    mq.addEventListener("change", handleChange);
+
+    return function cleanup() {
+        disable();
+        mq.removeEventListener("change", handleChange);
     };
 }
