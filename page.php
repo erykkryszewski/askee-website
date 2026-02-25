012b7f5265518534175a1963a3d7c44d4e2346fc
Eryk Kryszewski <eric9972@o2.pl>
2026-02-25 17:38:07 +0100

    site progress

--- page.php
+++ page.php
@@ -7,4 +7,16 @@ $url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 
 ?>
 
+<div class="askee-chat askee-privacy">
+    <div class="container-fluid container-fluid--padding" data-askee-page="contact" data-askee-topic="kontakt">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2 askee-chat__column askee-chat__column--mid">
+                <div class="askee-chat__wrapper"><?php echo apply_filters('the_title', get_the_content()); ?></div>
+            </div>
+
+            <div class="col-12 col-lg-2 askee-chat__column askee-chat__column--right"></div>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
